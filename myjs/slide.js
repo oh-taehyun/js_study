10a158207851cd13ae484e927b0ca008af8b955d
oh-taehyun <xogus7038@naver.com>
2023-07-31 23:53:01 +0900

    자바스크립트 슬라이드 구현(3)-슬라이드 이동, 버튼 비활성화 함수

--- myjs/slide.js
+++ myjs/slide.js
@@ -4,7 +4,7 @@ var sliderWrapper = document.getElementsByClassName('container'),
     sliderContainer = document.getElementsByClassName('slider-container'),
     slides = document.getElementsByClassName('slide'),
     slideCount = slides.length,//슬라이드의 개수
-    currenIndex = 0,
+    currentIndex = 0,
     topHeight = 0,
     navPrev = document.getElementById('prev'), //아이디 prev
     navNext = document.getElementById('next');//아이디 next
@@ -35,15 +35,56 @@ for (var i = 0; i < slideCount; i++) {
 }
 
 // 슬라이드 이동 함수
+function goToSlide(idx) {
+    sliderContainer[0].style.left = idx * -100 + '%';
+    sliderContainer[0].classList.add('animated');
+    currentIndex = idx;
 
+    //updateNav();
+}
 
 
 // 버튼기능 업데이트 함수
-
+function updateNav() {
+    //처음일때
+    if (currentIndex == 0) {
+        navPrev.classList.add('disabled');
+    } else {
+        navPrev.classList.remove('disabled');
+    }
+    //마지막일때
+    if (currentIndex == slideCount - 1) {
+        navNext.classList.add('disabled');
+    } else {
+        navNext.classList.remove('disabled');
+    }
+}
 
 // 버튼을 클릭하면 슬라이드 이동시키기
 
+navPrev = addEventListener('click', function (e) {
+    e.preventDefault();
+    //goToSlide(currentIndex - 1);
+    //처음이 아니라면 goToSlide(currentIndex - 1);
+    //처음이라면 goToSlide(slideCount - 1);
+    if (currentIndex > 0) {
+        goToSlide(currentIndex - 1);
+    } else {
+        goToSlide(slideCount - 1);
+    }
+
+});
+
+navNext = addEventListener('click', function (e) {
+    e.preventDefault();
+    //goToSlide(currentIndex + 1);
+    if (currentIndex < slideCount - 1) {
+        goToSlide(currentIndex - 1);
+    } else {
+        goToSlide(0);
+    }
+});
 
 // 첫번째 슬라이드 먼저 보이도록 하기
-
+goToSlide(0);
 
